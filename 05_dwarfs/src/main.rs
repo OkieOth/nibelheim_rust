890837f078888caddce68620f45284d1f7764cb7
Eiko Thomas <eiko.thomas@lisaplus.de>
2021-03-30 00:21:22 +0200

    first steps

--- 05_dwarfs/src/main.rs
+++ 05_dwarfs/src/main.rs
@@ -3,52 +3,11 @@ Mine simulator .. the mine is represented by an array
 ... a really poor one :D
 */
 
-use rand::{thread_rng, Rng};
+mod mine;
+
 use std::io;
 
-enum MineralType {
-    MITHRIL,
-    GOLD,
-    SILVER,
-    DIAMOND,
-    IRON,
-    CUPPER,
-    ROCK
-}
 
-struct MineSpot {
-    mineral: MineralType
-}
-
-fn init_gold_and_stuff() -> Option<MineSpot> {
-    if thread_rng().gen_range(0..2) == 1 {
-        // this spot has minerals
-        let spot: MineSpot = MineSpot {
-            mineral: match thread_rng().gen_range(0..5) {
-                0 => MineralType::MITHRIL,
-                1 => MineralType::GOLD,
-                2 => MineralType::SILVER,
-                3 => MineralType::DIAMOND,
-                4 => MineralType::IRON,
-                5 => MineralType::CUPPER,
-                _ => MineralType::ROCK
-            }
-        };
-        Some(spot)
-    }
-    else {
-        // this spot is empty
-        None
-    }
-}
-
-fn init_mine_with_gold(mine: &mut Vec<Option<MineSpot>>, mine_size: usize) {
-    println!("Current size of the mine: {}", mine.len());
-    for _index in 0..mine_size {        
-        let spot: Option<MineSpot> = init_gold_and_stuff();
-        mine.push(spot);
-    }
-}
     
 fn main() {
     let mut mine_size_str = String::new();
@@ -66,25 +25,25 @@ fn main() {
     }
    
     // this init sucks
-    let mut mine: Vec<Option<MineSpot>> = Vec::new();
+    let mut mine: Vec<Option<mine::MineSpot>> = Vec::new();
 
-    init_mine_with_gold(&mut mine, mine_size);
+    mine::init_mine_with_gold(&mut mine, mine_size);
 
     let mut output: String = "Mine [".to_owned();
 
     let last_index = mine.len() -1; 
     for index in 0..mine.len() {
-        let opt: Option<&MineSpot> = mine[index].as_ref();
+        let opt: Option<&mine::MineSpot> = mine[index].as_ref();
         if opt.is_some() {
             let spot = opt.unwrap();
             match spot.mineral {
-                MineralType::MITHRIL => output.push_str("M"), 
-                MineralType::GOLD => output.push_str("G"),
-                MineralType::SILVER => output.push_str("S"),
-                MineralType::DIAMOND => output.push_str("D"),
-                MineralType::IRON => output.push_str("I"),
-                MineralType::CUPPER => output.push_str("C"),            
-                MineralType::ROCK => output.push_str("_")            
+                mine::MineralType::MITHRIL => output.push_str("M"), 
+                mine::MineralType::GOLD => output.push_str("G"),
+                mine::MineralType::SILVER => output.push_str("S"),
+                mine::MineralType::DIAMOND => output.push_str("D"),
+                mine::MineralType::IRON => output.push_str("I"),
+                mine::MineralType::CUPPER => output.push_str("C"),            
+                mine::MineralType::ROCK => output.push_str("_")            
             }
         } else {
             output.push_str(" ");
@@ -109,7 +68,7 @@ mod tests {
         let mut found: bool = false;
 
         for _index in 0..10 {
-            let spot: Option<MineSpot> = init_gold_and_stuff();
+            let spot: Option<mine::MineSpot> = mine::init_gold_and_stuff();
             if spot.is_some() {
                 found = true
             }

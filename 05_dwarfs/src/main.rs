c4bb8a2c65c5bec49cd0ae5ec99ca2462e7c4320
Eiko Thomas <eiko.thomas@lisaplus.de>
2021-04-06 13:24:19 +0200

    move mine module to a sub dir

--- 05_dwarfs/src/main.rs
+++ 05_dwarfs/src/main.rs
@@ -3,7 +3,7 @@ Mine simulator .. the mine is represented by an array
 ... a really poor one :D
 */
 
-mod mine;
+mod modules;
 
 use std::io;
 
@@ -25,25 +25,25 @@ fn main() {
     }
    
     // this init sucks
-    let mut mine: Vec<Option<mine::MineSpot>> = Vec::new();
+    let mut mine: Vec<Option<modules::mine::MineSpot>> = Vec::new();
 
-    mine::init_mine_with_gold(&mut mine, mine_size);
+    modules::mine::init_mine_with_gold(&mut mine, mine_size);
 
     let mut output: String = "Mine [".to_owned();
 
     let last_index = mine.len() -1; 
     for index in 0..mine.len() {
-        let opt: Option<&mine::MineSpot> = mine[index].as_ref();
+        let opt: Option<&modules::mine::MineSpot> = mine[index].as_ref();
         if opt.is_some() {
             let spot = opt.unwrap();
             match spot.mineral {
-                mine::MineralType::MITHRIL => output.push_str("M"), 
-                mine::MineralType::GOLD => output.push_str("G"),
-                mine::MineralType::SILVER => output.push_str("S"),
-                mine::MineralType::DIAMOND => output.push_str("D"),
-                mine::MineralType::IRON => output.push_str("I"),
-                mine::MineralType::CUPPER => output.push_str("C"),            
-                mine::MineralType::ROCK => output.push_str("_")            
+                modules::mine::MineralType::MITHRIL => output.push_str("M"), 
+                modules::mine::MineralType::GOLD => output.push_str("G"),
+                modules::mine::MineralType::SILVER => output.push_str("S"),
+                modules::mine::MineralType::DIAMOND => output.push_str("D"),
+                modules::mine::MineralType::IRON => output.push_str("I"),
+                modules::mine::MineralType::CUPPER => output.push_str("C"),            
+                modules::mine::MineralType::ROCK => output.push_str("_")            
             }
         } else {
             output.push_str(" ");
@@ -68,7 +68,7 @@ mod tests {
         let mut found: bool = false;
 
         for _index in 0..10 {
-            let spot: Option<mine::MineSpot> = mine::init_gold_and_stuff();
+            let spot: Option<modules::mine::MineSpot> = modules::mine::init_gold_and_stuff();
             if spot.is_some() {
                 found = true
             }
